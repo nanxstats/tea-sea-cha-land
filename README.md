90c674b01d89830496de7fe6dd229c0e7214a019
Nan Xiao <road2stat@gmail.com>
2018-01-15 14:14:35 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# "Tea by Sea, Cha by Land" Dataset 🍵
+
+This repository archives a temporal-spatial dataset characterizing
+how the word "tea" (茶) spread over the land and sea.
+
+## Load the data
+
+To load the data, use the following command in R:
+
+```r
+df = read.csv("tea-sea-cha-land.csv")
+```
+
+The R code for tidying the data is in [data-generator.R](data-generator.R).
+
+## Data source
+
+[World Atlas of Language Structures (WALS) database. Feature 138A: Tea.](http://wals.info/feature/138A#2/25.5/143.6)
+
+## Read more
+
+[Tea if by sea, cha if by land: Why the world only has two words for tea](https://qz.com/1176962/map-how-the-word-tea-spread-over-land-and-sea-to-conquer-the-world/), by Nikhil Sonnad. January 11, 2018.
+
+[Tea if by Sea](languageoffood.blogspot.com/2014/08/tea-if-by-sea.html), by Dan Jurafsky. August 3, 2014.
---
+++ data-generator.R
@@ -0,0 +1,52 @@
+# Download and tidy the 'tea by sea, cha by land' data from the
+# World Atlas of Language Structures (WALS) database
+# http://wals.info/feature/138A#2/25.5/143.6
+
+# geolocation data
+download.file('http://wals.info/feature/138A.tab', '138A.tab')
+# annotation data
+download.file('https://raw.githubusercontent.com/clld/wals-data/master/cldf/feature-138A.cldf.csv', 'feature-138A.cldf.csv')
+
+df_geo = read.table('138A.tab', sep = '\t', skip = 7, header = TRUE, quote = '', as.is = TRUE)
+df_anno = read.csv('feature-138A.cldf.csv', as.is = TRUE)
+
+names(df_anno) = tolower(names(df_anno))
+
+# create index for both data frames
+row.names(df_anno) = substr(
+  df_anno[, 'id'], nchar(df_anno[, 'id']) - 2, nchar(df_anno[, 'id']))
+row.names(df_geo) = df_geo[, 'wals.code']
+
+# remove overlapped columns
+df_anno[, c('value', 'feature_id')] = NULL
+df_geo[, c('wals.code', 'name', 'area')] = NULL
+
+# merge two data frames
+df = merge(df_anno, df_geo, by = 'row.names')
+
+# rename columns
+colnames(df) = c(
+  'id', 'language', 'source',
+  'id_url', 'language_url', 'source_url',
+  'value', 'description', 'latitude', 'longitude', 'genus', 'family')
+
+# replace blanks with NA
+df[which(df[, 'source'] == ''), 'source'] = NA
+df[which(df[, 'source_url'] == ''), 'source_url'] = NA
+
+# create year column
+year = df[, 'source']
+year[!grepl('\\[', df[, 'source'])] = NA
+df[, 'year'] = sub('.*\\[ *(.*?) *\\].*', '\\1', year)
+
+# remove [year] in source
+df[, 'source'] = gsub('\\[.*\\]', '', df[, 'source'])
+
+# reorder columns
+df = df[c(
+  'id', 'language', 'value', 'description',
+  'year', 'latitude', 'longitude',
+  'genus', 'family', 'source',
+  'id_url', 'language_url', 'source_url')]
+
+write.csv(df, file = 'tea-sea-cha-land.csv', quote = FALSE, row.names = FALSE)
