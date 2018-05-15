8216ac9cbec4b40d17539a9de0465a59af36c05a
Nan Xiao <road2stat@gmail.com>
2018-05-15 02:41:59 -0400

    tidyverse coding style

--- data-generator.R
+++ data-generator.R
@@ -3,50 +3,53 @@
 # http://wals.info/feature/138A#2/25.5/143.6
 
 # geolocation data
-download.file('http://wals.info/feature/138A.tab', '138A.tab')
+download.file("http://wals.info/feature/138A.tab", "138A.tab")
 # annotation data
-download.file('https://raw.githubusercontent.com/clld/wals-data/master/cldf/feature-138A.cldf.csv', 'feature-138A.cldf.csv')
+download.file("https://raw.githubusercontent.com/clld/wals-data/master/cldf/feature-138A.cldf.csv", "feature-138A.cldf.csv")
 
-df_geo = read.table('138A.tab', sep = '\t', skip = 7, header = TRUE, quote = '', as.is = TRUE)
-df_anno = read.csv('feature-138A.cldf.csv', as.is = TRUE)
+df_geo <- read.table("138A.tab", sep = "\t", skip = 7, header = TRUE, quote = "", as.is = TRUE)
+df_anno <- read.csv("feature-138A.cldf.csv", as.is = TRUE)
 
-names(df_anno) = tolower(names(df_anno))
+names(df_anno) <- tolower(names(df_anno))
 
 # create index for both data frames
-row.names(df_anno) = substr(
-  df_anno[, 'id'], nchar(df_anno[, 'id']) - 2, nchar(df_anno[, 'id']))
-row.names(df_geo) = df_geo[, 'wals.code']
+row.names(df_anno) <- substr(
+  df_anno[, "id"], nchar(df_anno[, "id"]) - 2, nchar(df_anno[, "id"])
+)
+row.names(df_geo) <- df_geo[, "wals.code"]
 
 # remove overlapped columns
-df_anno[, c('value', 'feature_id')] = NULL
-df_geo[, c('wals.code', 'name', 'area')] = NULL
+df_anno[, c("value", "feature_id")] <- NULL
+df_geo[, c("wals.code", "name", "area")] <- NULL
 
 # merge two data frames
-df = merge(df_anno, df_geo, by = 'row.names')
+df <- merge(df_anno, df_geo, by = "row.names")
 
 # rename columns
-colnames(df) = c(
-  'id', 'language', 'source',
-  'id_url', 'language_url', 'source_url',
-  'value', 'description', 'latitude', 'longitude', 'genus', 'family')
+colnames(df) <- c(
+  "id", "language", "source",
+  "id_url", "language_url", "source_url",
+  "value", "description", "latitude", "longitude", "genus", "family"
+)
 
 # replace blanks with NA
-df[which(df[, 'source'] == ''), 'source'] = NA
-df[which(df[, 'source_url'] == ''), 'source_url'] = NA
+df[which(df[, "source"] == ""), "source"] <- NA
+df[which(df[, "source_url"] == ""), "source_url"] <- NA
 
 # create year column
-year = df[, 'source']
-year[!grepl('\\[', df[, 'source'])] = NA
-df[, 'year'] = sub('.*\\[ *(.*?) *\\].*', '\\1', year)
+year <- df[, "source"]
+year[!grepl("\\[", df[, "source"])] <- NA
+df[, "year"] <- sub(".*\\[ *(.*?) *\\].*", "\\1", year)
 
 # remove [year] in source
-df[, 'source'] = gsub('\\[.*\\]', '', df[, 'source'])
+df[, "source"] <- gsub("\\[.*\\]", "", df[, "source"])
 
 # reorder columns
-df = df[c(
-  'id', 'language', 'value', 'description',
-  'year', 'latitude', 'longitude',
-  'genus', 'family', 'source',
-  'id_url', 'language_url', 'source_url')]
+df <- df[c(
+  "id", "language", "value", "description",
+  "year", "latitude", "longitude",
+  "genus", "family", "source",
+  "id_url", "language_url", "source_url"
+)]
 
-write.csv(df, file = 'tea-sea-cha-land.csv', quote = FALSE, row.names = FALSE)
+write.csv(df, file = "tea-sea-cha-land.csv", quote = FALSE, row.names = FALSE)
--- plot-remap.R
+++ plot-remap.R
@@ -1,28 +1,32 @@
-library('dplyr')
-if (!require(REmap)) devtools::install_github('lchiffon/REmap')
-library('REmap')
+library("dplyr")
+if (!require("REmap")) devtools::install_github("lchiffon/REmap")
+library("REmap")
 
-dat = read.csv('https://raw.githubusercontent.com/road2stat/tea-sea-cha-land/master/tea-sea-cha-land.csv')
+dat <- read.csv("https://raw.githubusercontent.com/road2stat/tea-sea-cha-land/master/tea-sea-cha-land.csv")
 
 # remove missing data
-rawDat = dat %>% na.omit
+rawDat <- dat %>% na.omit()
 
-markPoint = rawDat %>% select(id,value)
-markPoint$color = ifelse(
-  markPoint$value == 1, 'red',
-  ifelse(markPoint$value == 2, 'blue', 'green'))
-geoData = rawDat %>% select(longitude,latitude,id)
+markPoint <- rawDat %>% select(id, value)
+markPoint$color <- ifelse(
+  markPoint$value == 1, "red",
+  ifelse(markPoint$value == 2, "blue", "green")
+)
+geoData <- rawDat %>% select(longitude, latitude, id)
 
 # first plot: without paths
-data = data.frame(country = mapNames('world'), value = 5 * sample(178) + 200)
+data <- data.frame(country = mapNames("world"), value = 5 * sample(178) + 200)
 
 head(data)
-out = remapC(
-  data,maptype = 'world', color = 'white',
+out <- remapC(
+  data,
+  maptype = "world", color = "white",
   markPointTheme = markPointControl(
-    effect = FALSE, symbol = 'pin', symbolSize = 5),
+    effect = FALSE, symbol = "pin", symbolSize = 5
+  ),
   markPointData = markPoint,
-  geoData = geoData)
+  geoData = geoData
+)
 plot(out)
 
 # second plot: with computed paths
@@ -31,31 +35,36 @@ plot(out)
 # type should be the same; year of the starting point
 # is smaller than that of the ending point; with the shortest path
 
-output = list()
-for(i in 1:nrow(rawDat)) {
-  tmpLine = rawDat[i, ]
+output <- list()
+for (i in 1:nrow(rawDat)) {
+  tmpLine <- rawDat[i, ]
   if (tmpLine$value == 3) next
-  tmpDat = rawDat %>% filter(value == tmpLine$value & year < tmpLine$year) %>%
+  tmpDat <- rawDat %>%
+    filter(value == tmpLine$value & year < tmpLine$year) %>%
     arrange((latitude - tmpLine$latitude)^2 + (longitude - tmpLine$longitude)^2)
   if (nrow(tmpDat) > 0) {
-    output = append(output,list(c(
+    output <- append(output, list(c(
       ori = as.character(tmpDat[1, ]$id),
       des = as.character(tmpLine$id),
-      value = tmpLine$value)))
+      value = tmpLine$value
+    )))
   }
 }
 
-markLineDat = output %>% do.call(rbind, .) %>% as.data.frame
-markLineDat$color = ifelse(
-  markLineDat$value == 1, 'red',
-  ifelse(markLineDat$value == 2, 'blue', 'green'))
+markLineDat <- output %>% do.call(rbind, .) %>% as.data.frame()
+markLineDat$color <- ifelse(
+  markLineDat$value == 1, "red",
+  ifelse(markLineDat$value == 2, "blue", "green")
+)
 
-out = remapB(
-  color = 'Blue',
+out <- remapB(
+  color = "Blue",
   markPointTheme = markPointControl(
-    effect = FALSE, symbol = 'pin', symbolSize = 5),
+    effect = FALSE, symbol = "pin", symbolSize = 5
+  ),
   markPointData = markPoint,
   markLineData = markLineDat,
   markLineTheme = markLineControl(lineWidth = 2),
-  geoData = geoData)
+  geoData = geoData
+)
 plot(out)
